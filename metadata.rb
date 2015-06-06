5372d03b3c5977a36b6616ae5f01efe1e1b24e4d
Matt Cole <matt-cole@hotmail.co.uk>
2015-06-06 12:58:29 +0100

    Added status to init.d script

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       'Matt Cole'
 maintainer_email 'matt-cole@hotmail.co.uk'
 license          'Apache 2.0'
 description      'Installs/Configures TeamCity'
-version          '0.1.0'
+version          '0.2.0'
 
 supports         'ubuntu'
 
